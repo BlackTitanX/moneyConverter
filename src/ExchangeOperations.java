9502d5a629b91ffb317e584be99e93f50e41be16
Israel <israelperezmasle2@gmail.com>
2024-04-27 12:10:40 -0400

    Almost done, messing testing and edge cases.

--- src/ExchangeOperations.java
+++ src/ExchangeOperations.java
@@ -7,4 +7,43 @@ import java.net.http.HttpResponse;
 
 public class ExchangeOperations {
 
+        ConvertionData getRate(String currency){
+
+
+            URI url = URI.create("https://v6.exchangerate-api.com/v6/e38f45b9b07f6e5f6de10308/latest/" + currency);
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder().uri(url).build();
+            HttpResponse <String> response = null;
+
+            try {
+                response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            } catch (IOException | InterruptedException e) {
+
+                throw new RuntimeException(e);
+
+            }
+
+
+            return new Gson().fromJson(response.body(), ConvertionData.class);
+        }
+
+ public double performConvertion(String curency, double amount, String localamount){
+            double convertion = 0.0;
+            ConvertionData rates = getRate(curency);
+
+            try {
+                double rate = rates.conversion_rates().get(curency);
+
+                convertion = rate * amount;
+
+            } catch ( Exception e){
+
+                System.out.println("An error has occured");
+            }
+             return  convertion;
+ }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,13 @@ public class Main {
         System.out.println("Welcome to the Exchange Converter, please choose one of the following options");
         System.out.println(" ");
         System.out.println(" ");
-        System.out.println("1. USD");
-        System.out.println("2. DOP");
-        System.out.println("3. EUR");
-        System.out.println("4. Exit Program");
+        System.out.println("1. Dolar ==>> Peso argentino");
+        System.out.println("2. Peso argentiono ==>> Dolar");
+        System.out.println("3. Dolar==>> Real brasilegno ");
+        System.out.println("4. Real Brasilegno ==>> Dolar ");
+        System.out.println("5. Dolar ==>> Peso colombiano");
+        System.out.println("6. Peso Colombiano ==>> Dolar");
+        System.out.println("7. Exit Program");
     }
 
     public static void main(String[] args) {
@@ -24,7 +27,58 @@ public class Main {
             showMenu();
             operation = scanner.nextInt();
 
+            if(operation == 1){
+                ExchangeOperations calculator = new ExchangeOperations();
+                double amount;
+                System.out.println("Enter Amount");
+                amount = scanner.nextDouble();
+                calculator.performConvertion("USD",amount,"ARS");
+
+            }
+
+            if(operation == 2){
+                ExchangeOperations calculator = new ExchangeOperations();
+                double amount;
+                System.out.println("Enter Amount");
+                amount = scanner.nextDouble();
+                calculator.performConvertion("ARS",amount,"USD");
+
+            }
+
+            if(operation == 3){
+                ExchangeOperations calculator = new ExchangeOperations();
+                double amount;
+                System.out.println("Enter Amount");
+                amount = scanner.nextDouble();
+                calculator.performConvertion("USD",amount,"BRL");
+            }
+
             if(operation == 4){
+                ExchangeOperations calculator = new ExchangeOperations();
+                double amount;
+                System.out.println("Enter Amount");
+                amount = scanner.nextDouble();
+                calculator.performConvertion("BRL",amount,"USD");
+            }
+
+            if(operation == 5){
+                ExchangeOperations calculator = new ExchangeOperations();
+                double amount;
+                System.out.println("Enter Amount");
+                amount = scanner.nextDouble();
+                calculator.performConvertion("USD",amount,"COP");
+            }
+
+            if(operation == 6){
+                ExchangeOperations calculator = new ExchangeOperations();
+                double amount;
+                System.out.println("Enter Amount");
+                amount = scanner.nextDouble();
+                calculator.performConvertion("COP",amount,"USD");
+            }
+
+
+            if(operation == 7){
                 keepRunning = false;
                 System.out.println("Thank you for using our system");
             }
